dc1e38f7933adeb385da312c1bceb7acf68bed35
ht <hikmet@texmart.com>
2025-11-29 17:11:36 +0300

    Fix: Remove nested git repository to resolve Netlify submodule error

---
+++ zaman makinesi/zaman makinesi/zaman makinesi/zaman_makinesiApp.swift
@@ -0,0 +1,17 @@
+//
+//  zaman_makinesiApp.swift
+//  zaman makinesi
+//
+//  Created by hikmet tanriverdi on 23.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct zaman_makinesiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
